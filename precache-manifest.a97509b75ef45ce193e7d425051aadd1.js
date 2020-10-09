abb8f1c80ca99305a8b34ae3981b8bf58943816f
Yaron <yaronl219@gmail.com>
2020-10-09 14:21:49 +0300

    added build folder

---
+++ precache-manifest.a97509b75ef45ce193e7d425051aadd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc0ea4899cbf129a2ef3b40f0ce2130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f12920c51a56472ac33d",
+    "url": "/static/css/main.2b4a89a0.chunk.css"
+  },
+  {
+    "revision": "ec7a88efb84666988ac2",
+    "url": "/static/js/2.1ba26afa.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.1ba26afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12920c51a56472ac33d",
+    "url": "/static/js/main.7867452f.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "1ecdd7e1a9c87160eeb1a65d86085882",
+    "url": "/static/media/logo.1ecdd7e1.svg"
+  },
+  {
+    "revision": "cf7355315ccd96d80a3118cff78384ac",
+    "url": "/static/media/weather.cf735531.svg"
+  }
+]);
